6ae849c7b1d34c5c6ec14997e825e89bb72a3906
YH <YH@DESKTOP-GDR6S4Q>
2018-01-30 23:47:45 +0900

    list

--- simple/src/main/java/si/mp/le/dao/UserDAO.java
+++ simple/src/main/java/si/mp/le/dao/UserDAO.java
@@ -3,6 +3,7 @@ package si.mp.le.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,56 +11,97 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.iot.test.vo.UserClass;
-
 import si.mp.le.common.DBCon;
 import si.mp.le.vo.User;
 
 @Repository
 public class UserDAO {
-	
+
 	@Autowired
 	private BasicDataSource bc;
 	@Autowired
 	private DBCon dbc;
 	@Autowired
 	private User user;
-	
-	public List<User> userList(){
-		List<User> list = new ArrayList<User>();
-		return userList();
-	}
+
 	public List<User> selectUserList() {
 		List<User> list = new ArrayList<User>();
-    	String sql = "select * from user_info uino=1";	
-    	Connection con = null;
+		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-		
 		try {
-			con = bc.getConnection();
+			con = bc.getConnection(); 
+			String sql = "select * from user_info where 1=1";
 			ps = con.prepareStatement(sql);
 			rs = ps.executeQuery();
 			while (rs.next()) {
-				UserClass uc = new UserClass();
-				uc.setAddress(rs.getString("address"));
-				uc.setCiDesc(rs.getString("cidesc"));
-				uc.setCiName(rs.getString("ciname"));
-				uc.setCiNo(rs.getInt("cino"));
-				uc.setUiAge(rs.getInt("uiage"));
-				uc.setUiId(rs.getString("uiid"));
-				uc.setUiName(rs.getString("uiname"));
-				uc.setUiNo(rs.getInt("uino"));
-				uc.setUiPwd(rs.getString("uipwd"));
-				uc.setUiRegdate(rs.getString("uiregdate"));
-				userList.add(uc);
-			
+				User user = new User();
+				user.setAddress(rs.getString("address"));
+				user.setUiAge(rs.getInt("uiage"));
+				user.setUiId(rs.getString("uiid"));
+				user.setUiName(rs.getString("uiname"));
+				user.setUiNo(rs.getInt("uino"));
+				user.setUiPwd(rs.getString("uipwd"));
+				list.add(user);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		System.out.println(dbc.getConnection());
-		user.setName("홍길동");
-		user.setAge(30);
-		return selectuser;
-	
+		return list;
 	}
 	
+	public int inserUser() {
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = bc.getConnection(); 
+			String sql = "insert into user_info(uiname,uino,uiage,uiid,uipwd,address)\r\n"
+					+ "values(?,?,?,?,?,?) ";
+			ps = con.prepareStatement(sql);				
+				ps.setString(1,user.getAddress());
+				ps.setInt(2, user.getUiNo());
+				ps.setString(3, user.getUiName());
+				ps.setString(4, user.getUiId());
+				ps.setInt(5, user.getUiAge());
+				ps.setString(6, user.getUiPwd());
+				return ps.executeUpdate();
+			}catch (SQLException e) {
+				e.printStackTrace();
+		} 
+		return 0;
+		}
+		
+	public int updateUser() {
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = bc.getConnection();
+			String sql = "update user_info set uiname=?,uiage=?,address=? where uino=?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, user.getUiName());
+			ps.setInt(2, user.getUiAge());
+			ps.setString(3, user.getAddress());
+			ps.setInt(4, user.getUiNo());
+			return ps.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+		
+	}
+	
+	public int deleteUser() {
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = bc.getConnection();
+			String sql = "delete from user_info where uino=?";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, user.getUiNo());
+			return ps.executeUpdate();
+		}catch (SQLException e) {
+			e.printStackTrace();
+	}
+	return 0;
+	}
 }
--- simple/src/main/java/si/mp/le/service/UserSerivce.java
+++ simple/src/main/java/si/mp/le/service/UserSerivce.java
@@ -1,5 +1,7 @@
 package si.mp.le.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,7 @@ public class UserSerivce {
 	@Autowired
 	private UserDAO udao;
 	
-	public User getUser() {
-		return udao.selectUser();
+	public List<User> getUser() {
+		return udao.selectUserList();
 	}
 }
